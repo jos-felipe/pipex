024ea1767a4714be621f02be9c0bdeece8e9c3d3
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2023-12-18 15:49:59 -0300

    new file:   include/pipex.h

---
+++ include/pipex.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 15:38:33 by josfelip          #+#    #+#             */
+/*   Updated: 2023/12/18 15:47:32 by josfelip         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h>
+
+typedef enum e_bool
+{
+	FALSE,
+	TRUE,
+}		t_bool;
+
+typedef enum e_proc
+{
+	INITIAL,
+	FINAL,
+}		t_proc;
+
+typedef struct s_cmd
+{
+	pid_t	pid;
+	t_proc	proc_type;
+	char	*cmd;
+	char	**argv;
+	char	**envp;
+}			t_cmd;
+
+typedef struct s_pipex
+{
+	int		fd_input_file;
+	int		fd_output_file;
+	int		fd_pipe[2];
+	size_t	commands;
+	t_cmd	*commands_array;
+	t_bool	success;
+}			t_pipex;
+
+#endif
