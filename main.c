499bc4a3abaee30344a2f6489ce1b62a3b5b55ce
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-01-16 18:05:40 -0300

    Update .gitignore and launch.json, fix bug in main.c

--- main.c
+++ main.c
@@ -9,19 +9,54 @@ int	main(int argc, char *argv[]) {
 	int	in;
 	int	out;
 	int	fd[2];
+	char	*infile;
+	char	*outfile;
 
+	if (argc == 5)
+	{
+		infile = argv[1];
+		outfile = argv[4];
+	}
+	else
+	{
+		printf("Usage: ./pipex file1 cmd1 cmd2 file2\n");
+		exit(1);
+	}
+	
+	// Sanity checks on user input
+	if (access(infile, F_OK) == -1) 
+	{
+		perror(infile);
+		exit(1);
+	}
+	else if (access(infile, R_OK) == -1) 
+	{
+		perror(infile);
+		exit(1);
+	}
+	
 	// Creates the news file descriptors
-	in = open("infile", O_RDONLY);
-	out = open("outfile", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	in = open(infile, O_RDONLY);
+	if (in == -1) 
+	{
+		perror(infile);
+		exit(3);
+	}
+	out = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (out == -1) 
+	{
+		perror(outfile);
+		exit(3);
+	}
 	if (pipe(fd) == -1) {
 		return (1);
 	}
 
+	// cmd1
 	int pid1 = fork();
 	if (pid1 < 0) {
 		return 2;
 	}
-
 	if (pid1 == 0) {
 		dup2(in, STDIN_FILENO);
 		dup2(fd[1], STDOUT_FILENO);
@@ -30,6 +65,7 @@ int	main(int argc, char *argv[]) {
 		execlp("cat", "cat", NULL);
 	}
 
+	// cmd2
 	int pid2 = fork();
 	if (pid2 < 0) {
 		return 3;
@@ -40,7 +76,7 @@ int	main(int argc, char *argv[]) {
 		dup2(out, STDOUT_FILENO);
 		close(fd[0]);
 		close(fd[1]);
-		execlp("grep", "grep", "flag", NULL);
+		execlp("grep", "grep", "call", NULL);
 	}
 
 	close(out);
